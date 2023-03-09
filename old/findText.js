b2e3ab2ec7adbd9126c10bea1c12fe908ad76ab8
apache0505 <jiayulutsk@ukr.net>
2023-03-09 20:57:53 +0200

    as

--- old/findText.js
+++
@@ -1,6 +0,0 @@
-function findText(text, find) {
-  // put your code here
-  return text.toLowerCase().includes(find.toLowerCase());
-}
-
-console.log(findText('JavaScript String toUpperCase', 'vaScript'));
--- old/textContains.js
+++
@@ -1,7 +0,0 @@
-function findText(text) {
-  // put your code here
-  //eturn text.toLowerCase().includes(find.toLowerCase());
-  return text.toLowerCase() === text.toUpperCase();
-}
-
-console.log(findText('.'));
